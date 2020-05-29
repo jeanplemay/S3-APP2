d7a7ab4ff21d4e7f96a95bc5d815ee90bc84bb5e
JPLEMAY <info@jplemay.com>
2020-05-29 16:34:19 -0400

    Menu et barre d'outils

--- e04p1/src/main/java/e04p1/FactoryController.java
+++ e04p1/src/main/java/e04p1/FactoryController.java
@@ -4,44 +4,86 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class FactoryController {
 
     @FXML
-    private TextField heightField;
+    private MenuItem menuDelete;
 
     @FXML
-    private TextField widthField;
+    private Button buttonFullScreen;
 
     @FXML
-    private TextField areaText; 
+    private MenuItem menuClose;
 
     @FXML
-    private Label areaLabel;
+    private VBox vboxLeft;
 
     @FXML
-    private Label widthLabel;
+    private TitledPane titledPane1;
 
     @FXML
-    private Label heightLabel;
-    
+    private TitledPane titledPane2;
+
     @FXML
-    private Button rectangleButton;
-    
+    private VBox vboxTop;
+
     @FXML
-    private Button circleButton;
-    
+    private TitledPane titledPane3;
+
     @FXML
-    private void circleButtonClicked(ActionEvent event) {
-    	
-    	
+    private TitledPane titledPane4;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Label labelModes;
+
+    @FXML
+    private Label labelStatusBar;
+
+    @FXML
+    private Pane paneDessin;
+
+    @FXML
+    private HBox hBoxOutils;
+
+    @FXML
+    private Label labelOptions;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    void menuCloseClicked(ActionEvent event) {
+    	labelStatusBar.setText("Fermeture de l'application...");
+    }
+
+    @FXML
+    void menuDeleteClicked(ActionEvent event) {
+    	labelStatusBar.setText("Supression de l'élément...");
     }
     
     @FXML
-    private void rectangleButtonClicked(ActionEvent event) {
-    	
+    void buttonAddClicked(ActionEvent event) {
+    	labelStatusBar.setText("Ajout d'un élément...");
     }
-    
+
+    @FXML
+    void buttonFullScreenClicked(ActionEvent event) {
+    	labelStatusBar.setText("Activation du mode plein écran...");
+    }
+
 }
+
+
+    
+
+
